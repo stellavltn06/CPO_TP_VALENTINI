d4d2ade336841a46898e6433ad46139c358dddb0
stell <stell@PC_STELLA>
2025-12-12 14:20:52 +0100

    Ajout de mini projet 1 Valentini

---
+++ Mini Projet Cadenas_Valentini_Stella/src/mini/projet/cadenas_valentini_stella/MiniProjetCadenas_Valentini_Stella.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mini.projet.cadenas_valentini_stella;
+
+/**
+ *
+ * @author stell
+ */
+public class MiniProjetCadenas_Valentini_Stella {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Interface_Cadenas().setVisible(true);
+    }
+    
+}
